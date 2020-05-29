5927fd07f59ddc77ed43d3433ddd0890eb8388ef
prtk5436 <prtk5436@users.noreply.github.com>
2020-05-28 19:22:41 -0700

    viewpager removed

--- src/main/java/com/example/mymp3/MainActivity.java
+++ src/main/java/com/example/mymp3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mymp3;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -8,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
@@ -40,14 +40,42 @@ public class MainActivity extends AppCompatActivity {
 
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = findViewById(R.id.viewpager);
-
-        // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
-
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
+        Button hindi = findViewById(R.id.hindi);
+        hindi.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent instrumentIntent = new Intent(MainActivity.this, HindiSongActivity.class);
+                startActivity(instrumentIntent);
+            }
+        });
+        Button marathi = findViewById(R.id.Marathi);
+        marathi.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent instrumentIntent = new Intent(MainActivity.this, MarathiSongsActivity.class);
+                startActivity(instrumentIntent);
+            }
+        });
+        Button Panjabi = findViewById(R.id.Panjabi);
+        Panjabi.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent instrumentIntent = new Intent(MainActivity.this, PanjabiSongsActivity.class);
+                startActivity(instrumentIntent);
+            }
+        });
+        Button English = findViewById(R.id.english);
+        English.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent instrumentIntent = new Intent(MainActivity.this, EnglishSongActivity.class);
+                startActivity(instrumentIntent);
+            }
+        });
 
         Button play = findViewById(R.id.play_pause);
         play.setOnClickListener(new View.OnClickListener() {
